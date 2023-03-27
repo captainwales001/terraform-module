fe8f63ed526e0be26a68ccc62908155a42775b7d
Captain Wales <adewalecoker001@gmail.com>
2023-03-27 14:46:06 +0100

    created VPC modules

---
+++ vpc/variables.tf
@@ -0,0 +1,21 @@
+# environment variables
+variable "region" {}
+variable "project_name" {}
+variable "environment" {}
+
+# vpc variable
+variable "vpc_cidr" {}
+variable "public_subnet_az1_cidr" {}
+variable "public_subnet_az2_cidr" {}
+variable "private_app_subnet_az1_cidr" {}
+variable "private_app_subnet_az2_cidr" {}
+variable "private_data_subnet_az1_cidr" {}
+variable "private_data_subnet_az2_cidr" {}
+
+
+
+
+
+
+
+
