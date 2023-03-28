8b1a363b449a8f0c40b7b0d783d21fe0f8d04e4f
Captain Wales <adewalecoker001@gmail.com>
2023-03-28 14:45:34 +0100

    commited nat-gateway

--- nat-gateway/main.tf
+++ nat-gateway/main.tf
@@ -86,12 +86,12 @@ resource "aws_route_table" "private_route_table_az2" {
 
 # associate private app subnet az2 with private route table az2
 resource "aws_route_table_association" "private_app_subnet_az2_rt_az2_association" {
-  subnet_id      = "var.private_app_subnet_az2_id"
+  subnet_id      = var.private_app_subnet_az2_id
   route_table_id = aws_route_table.private_route_table_az2.id
 }
 
 # associate private data subnet az2 with private route table az2
 resource "aws_route_table_association" "private_data_subnet_az2_rt_az2_association" {
-  subnet_id      = "var.private_data_subnet_az2_id"
+  subnet_id      = var.private_data_subnet_az2_id
   route_table_id = aws_route_table.private_route_table_az2.id
 }
